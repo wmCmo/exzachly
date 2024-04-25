65dcaea70fec537bfedbcc17a52967f119bf1352
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-04-26 00:58:45 +0900

    Add tools

---
+++ src/app/tools/qr/page.tsx
@@ -0,0 +1,8 @@
+export default function qr() {
+    return (
+        <>
+            <h1 className="text-vw leading-none sm:text-7xl">Free QR Code Generator</h1>
+            <h4 className="text-xl my-4">Tired of paying for QR Code? This one is for you.</h4>
+        </>
+    )
+}
