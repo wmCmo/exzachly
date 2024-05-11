c1a2c7f2f65efa23c112cf449f7380d09d7e24fc
Zachariah Prajeeyachat <51499433+wmCmo@users.noreply.github.com>
2024-05-12 00:56:53 +0900

    Remove console.log in Nav component

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,14 +4,13 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Nav() {
-    const pages = ['home', 'tools', 'notion', 'community', 'portfolio']
+    const pages = ['home', 'tools', 'notion', 'community', 'portfolio'];
 
     const pathname = usePathname();
-    console.log(pathname)
 
     const mappedPages = pages.map(page_ => {
         return (
-            <Link href={'/' + (page_ === 'home' ? '' : page_)} key={page_}>
+            <Link href={page_ === 'home' ? '' : page_} key={page_}>
                 <li className={
                     `${(pathname.includes('/' + ((page_ === 'home') && (pathname=='/') ? '' : page_))) ? 'font-bold' : ''} transition-transform ease-in-out hover:underline underline-offset-8 duration-300 text-center`
                 }>{page_.charAt(0).toUpperCase() + page_.slice(1)}</li>
