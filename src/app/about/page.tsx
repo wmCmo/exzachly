2da465e6bcdb95d8b537f851280428bb6e0385ca
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-08-16 03:36:05 +0700

    style: fix dark style in /about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,15 +5,15 @@ export default function page() {
     const url = 'https://exzachly.vercel.app/contact.vcf';
 
     return (
-        <div className='flex flex-col items-center gap-8 mx-5'>
-            <h1 className='text-vw leading-none sm:text-7xl font-bold'>My Digital Contact</h1>
-            <div className="bg-white flex justify-center items-center rounded-xl relative">
+        <div className='flex flex-col items-center gap-8 mx-5 text-neutral-800 dark:text-neutral-200'>
+            <h1 className='text-vw leading-none sm:text-7xl font-bold '>My Digital Contact</h1>
+            <div className="dark:bg-white bg-neutral-100 flex justify-center items-center rounded-xl relative">
                 <Image src={'/contact-qr.svg'} height={300} width={300} alt='My VCF file download file as a qrcode' />
                 <div className='absolute w-12 h-12 rounded-xl bg-[#171717] text-3xl flex items-center justify-center'>
                     <Image src={'/green-book.svg'} width={32} height={32} alt='Microsoft fluent ui green book emoji' />
                 </div>
             </div>
-            <a href={url} className='bg-neutral-950 px-6 py-4 rounded-xl hover:-translate-y-1 transition-all duration-200 ease-in-out hover:bg-neutral-900 font-bold'>Download Contact File</a>
+            <a href={url} className='bg-neutral-50 hover:bg-white dark:bg-neutral-950 px-6 py-4 rounded-xl hover:-translate-y-1 transition-all duration-200 ease-in-out dark:hover:bg-neutral-900 font-bold'>Download Contact File</a>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Tools from "./tools/page"
+import Tools from "./tools/page";
 
 export default function Home() {
 
@@ -7,5 +7,5 @@ export default function Home() {
       <h1 className="text-neutral-800 dark:text-neutral-200 mt-32 mb-10 text-vw leading-none sm:text-8xl font-black">It&apos;s me, <span className="text-lime-500">Zach.</span></h1>
       <Tools />
     </main>
-  )
+  );
 }
