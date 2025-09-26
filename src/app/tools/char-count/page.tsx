68bafd4deb1cbffe5d9161aac9ff824f6620afc0
JuthathipLoedsinaudom <59654072+jjennyzs@users.noreply.github.com>
2025-09-26 23:37:30 +0900

    correct the component name from ChartCount to CharCount

--- src/app/tools/char-count/page.tsx
+++ src/app/tools/char-count/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 
-export default function ChartCount() {
+export default function CharCount() {
     const [text, setText] = useState('')
 
     const handleChange = (e: { target: { value: string } }) => {
