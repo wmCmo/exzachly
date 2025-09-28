fcf3bb246a18b2bb8ad03e2e35564559aee51344
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-09-29 01:10:51 +0900

    feat: add toggle button between counting words and characters in /char-count

--- src/app/components/TextArea.tsx
+++ src/app/components/TextArea.tsx
@@ -4,6 +4,7 @@ import { XCircle } from '@phosphor-icons/react/dist/ssr';
 import { TextAreaProps } from '../tools/char-count/page';
 
 export default function TextArea({ unique, value, text, setText }: { unique: number; value: TextAreaProps; text: TextAreaProps[]; setText: Dispatch<SetStateAction<TextAreaProps[]>>; }) {
+    const [isChar, setIsChar] = useState(true);
 
     const handleChange = (e: { target: { value: string; name: string; }; }) => {
         const value = e.target.value;
@@ -25,6 +26,16 @@ export default function TextArea({ unique, value, text, setText }: { unique: num
         }
     };
 
+    const countWords = () => {
+        const target = value.text;
+        if (!target) return 0;
+        const words = target.match(/ ./g);
+        if (words === null) {
+            return 1;
+        } else {
+            return words.length + 1;
+        }
+    };
 
     return (
         <div className='sm:max-w-full sm:mt-0 mt-4'>
@@ -35,7 +46,16 @@ export default function TextArea({ unique, value, text, setText }: { unique: num
                 </div>
                 <textarea onChange={handleChange} name="text" id="text" value={value.text} className="block max-w-full resize dark:bg-neutral-100 dark:text-neutral-800 rounded-b-md p-4 min-w-full sm:min-w-96 min-h-32"></textarea>
             </div>
-            <h3 className='mt-2'><b>Total:</b> {value.text.length} characters</h3>
+            <div className="flex justify-between items-center pt-2">
+                <h3 className='mt-2'><b>Total:</b> {isChar ? value.text.length : countWords()} {isChar ? 'characters' : 'words'}</h3>
+                <div className=''>
+                    <div className='relative flex gap-4 bg-neutral-600 dark:bg-neutral-900 rounded-md items-center cursor-pointer'>
+                        <p onClick={() => setIsChar(false)} className={`px-4 py-2 z-10 ${isChar ? 'text-white' : 'font-bold'} transition-all`}>Word</p>
+                        <p onClick={() => setIsChar(true)} className={`px-4 py-2 z-10 ${isChar ? 'font-bold' : 'text-white'} transition-all`}>Char</p>
+                        <div onClick={() => setIsChar(prev => !prev)} className={`top-0 left-0 absolute transition-transform duration-200 ${isChar ? 'translate-x-full w-20' : 'translate-x-0 w-20'} rounded-md h-10 bg-neutral-100 dark:bg-neutral-700`}></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
