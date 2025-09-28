5e63ab7f607693822f87df84647f8cef391640ae
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-09-28 23:39:54 +0900

    feat: add save to localStorage functioality + reset button for char-count

--- src/app/components/TextArea.tsx
+++ src/app/components/TextArea.tsx
@@ -1,27 +1,25 @@
 'use client';
 import { Dispatch, SetStateAction, useState } from 'react';
 import { XCircle } from '@phosphor-icons/react/dist/ssr';
+import { TextAreaProps } from '../tools/char-count/page';
 
-export default function TextArea({ unique, text, setText }: { unique: number; text: string[]; setText: Dispatch<SetStateAction<string[]>>; }) {
+export default function TextArea({ unique, value, text, setText }: { unique: number; value: TextAreaProps; text: TextAreaProps[]; setText: Dispatch<SetStateAction<TextAreaProps[]>>; }) {
 
-    const [fileName, setFileName] = useState('Untitled');
-
-    const handleChange = (e: { target: { value: string; }; }) => {
+    const handleChange = (e: { target: { value: string; name: string; }; }) => {
         const value = e.target.value;
+        const name = e.target.name;
         setText(prev => {
             const newArr = [...prev];
-            newArr[unique] = value;
+            newArr[unique] = { ...newArr[unique], [name]: value };
             return newArr;
         });
     };
 
     const handleDelete = () => {
-        console.log(unique);
         if (text.length > 0) {
             setText(prev => {
                 const newArr = [...prev];
                 newArr.splice(unique, 1);
-                console.log(newArr);
                 return newArr;
             });
         }
@@ -32,12 +30,12 @@ export default function TextArea({ unique, text, setText }: { unique: number; te
         <div className='sm:max-w-full sm:mt-0 mt-4'>
             <div className='dark:bg-neutral-800 rounded-xl bg-neutral-500'>
                 <div className="flex justify-center px-2 py-1 items-center relative">
-                    <input type="text" value={fileName} className='dark:bg-neutral-800 dark:text-neutral-400 text-white bg-neutral-500 text-center font-bold rounded-full' onChange={(e) => setFileName(e.target.value)} />
+                    <input type="text" name='file' value={value.file} className='dark:bg-neutral-800 dark:text-neutral-400 text-white bg-neutral-500 text-center font-bold rounded-full' onChange={handleChange} />
                     {(text.length > 1) && <XCircle fill='white' weight='fill' onClick={handleDelete} width={'1.2em'} height={'1.2em'} className='cursor-pointer absolute right-2 dark:fill-[#f56c7f]' alt='Delete' />}
                 </div>
-                <textarea onChange={handleChange} name="text" id="text" value={text[unique]} className="block max-w-full resize dark:bg-neutral-100 dark:text-neutral-800 rounded-b-md p-4 min-w-full sm:min-w-96 min-h-32"></textarea>
+                <textarea onChange={handleChange} name="text" id="text" value={value.text} className="block max-w-full resize dark:bg-neutral-100 dark:text-neutral-800 rounded-b-md p-4 min-w-full sm:min-w-96 min-h-32"></textarea>
             </div>
-            <h3 className='mt-2'><b>Total:</b> {text[unique].length} characters</h3>
+            <h3 className='mt-2'><b>Total:</b> {value.text.length} characters</h3>
         </div>
     );
 };
--- src/app/tools/char-count/page.tsx
+++ src/app/tools/char-count/page.tsx
@@ -1,26 +1,56 @@
 'use client';
 
-import { useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import TextArea from "@/app/components/TextArea";
 
+export interface TextAreaProps {
+    file: string;
+    text: string;
+}
+
 export default function CharCount() {
-    const [text, setText] = useState<string[]>(['']);
+    const emptyTextArea = { file: 'Untitled', text: '' };
+    const [text, setText] = useState<TextAreaProps[]>([emptyTextArea]);
+
+    useEffect(() => {
+        const stored = localStorage.getItem('history');
+        if (stored) setText(JSON.parse(stored));
+    }, []);
 
     const handleAdd = () => {
         setText(prev => {
             const newArr = [...prev];
-            newArr.push('');
+            newArr.push(emptyTextArea);
             return newArr;
         });
+    };
+
+    const handleSave = () => {
+        localStorage.setItem('history', JSON.stringify(text));
+    };
+
+    const handleReset = () => {
+        setText([emptyTextArea]);
+        handleSave();
+    };
+
+    const Button = ({ main, handleClick, children }: { main: boolean; handleClick: () => void; children: ReactNode; }) => {
+        return <button onClick={handleClick} className={`${main ? 'bg-lime-600 hover:bg-lime-500' : 'dark:bg-neutral-800 dark:hover:bg-neutral-700'} transition-colors duration-100 py-1 text-white px-4 rounded-md font-bold`}>{children}</button>;
     };
 
     return (
         <>
             <h1 className="text-vw leading-none sm:text-7xl font-bold">Flexible Text Counter</h1>
             <h4 className="text-xl my-4">Layout-free text counter. Designed for you.</h4>
-            <button onClick={handleAdd} className="bg-lime-600 py-1 text-white px-4 rounded-md font-bold mt-4 mb-8">New Text Box</button>
+            <div className="sm:flex items-center mt-4 mb-8 gap-4">
+                <Button main={true} handleClick={handleAdd}>New Text Box</Button>
+                <div className="flex items-center gap-2 mt-4 sm:mt-0">
+                    <Button main={false} handleClick={handleSave}>Save</Button>
+                    <Button main={false} handleClick={handleReset}>Reset</Button>
+                </div>
+            </div>
             <div className="-mt-4 sm:flex gap-4 flex-wrap">
-                {text.map((_, index) => <TextArea key={index} unique={index} text={text} setText={setText} />)}
+                {text.map((value, index) => <TextArea key={index} unique={index} value={value} setText={setText} text={text} />)}
             </div>
         </>
     );
